084e73066f33b21e813baae008010e39a00852aa
lakshit616 <lakshit1304khandelwal@gmail.com>
2025-06-23 11:55:54 +0530

    sign up page with credentials validation is completed

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -13,7 +13,12 @@ export class Signup {
   password = signal('');
 
   isFormValid = computed(() =>
-    this.email().includes('@') && this.password().length >= 6
+    this.email().includes('@') && 
+  this.password().length >= 6 && 
+  this.hasLowercase(this.password()) && 
+  this.hasUppercase(this.password()) && 
+  this.hasSymbol(this.password()) && 
+  this.hasNumber(this.password())
   );
 
   signup() {
@@ -24,4 +29,20 @@ export class Signup {
     localStorage.setItem('user', JSON.stringify(user));
     alert('Account created successfully!');
   }
+ hasUppercase(p: string): boolean {
+  return /[A-Z]/.test(p);
+}
+
+hasLowercase(p: string): boolean {
+  return /[a-z]/.test(p);
+}
+
+hasNumber(p: string): boolean {
+  return /[0-9]/.test(p);
+}
+
+hasSymbol(p: string): boolean {
+  return /[!@#$%^&*(),.?":{}|<>]/.test(p);
+}
+
 }
