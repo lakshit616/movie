d55428af78c9c160249248eb11bbd670f7730abb
lakshit616 <lakshit1304khandelwal@gmail.com>
2025-06-25 21:34:30 +0530

    header component

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {  RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+constructor(private router: Router) {}
+  //  checkLogin(){
+  //   const loginToHome = localStorage.getItem('loginToHome')
+  //   if(this.loginToHome.loginToHome==='true'){
+  //     return true;
+  //   }
+  //  }
+  checkHome(event:Event){
+    if (this.router.url==='/home'){
+      this.scrollToSection('main-section');
+    }
+    else{
+      this.router.navigate(['/']);
+    }
+  }
+  
+
+  // Contact scroll (always on same page)
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
 }
