e49a830e50fd85bcc26875b63c63d5cb34a45d10
lakshit616 <lakshit1304khandelwal@gmail.com>
2025-06-25 21:35:52 +0530

    home component complete

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
+import { Contact } from '../contact/contact';
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header,Contact],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home  {
 
 }
