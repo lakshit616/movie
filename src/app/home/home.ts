58b6cb8e82e33fb30767ffd1f63b373051e9bc94
lakshit616 <lakshit1304khandelwal@gmail.com>
2025-06-30 17:14:58 +0530

    home updated

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,19 +1,39 @@
-import { Component,signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MovieService } from '../services/movie';  // ✅ Correct import
 import { Header } from '../header/header';
-import { Contact } from '../contact/contact';
-import { movies, Movie } from './movie-data';
+
+import { Omdb } from '../services/omdb';
+import { subscribeOn } from 'rxjs';
+
+
 @Component({
   selector: 'app-home',
-  imports: [Header,Contact],
+  standalone: true,
+  imports: [CommonModule, Header],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home  {
-movies = signal<Movie[]>(movies);
-selectedMovie = signal<Movie | null>(null);
+export class Home {
+  selectedMovie = signal<any | null>(null);
 
-selectMovie(movie: Movie) {
-    this.selectedMovie.set(movie);
+  constructor(public movieService: MovieService, private omdb:Omdb) {
+    this.movieService.fetchMovies(); // ✅ fetch from API on load
+  }
+
+  // ✅ Access movies as a getter to avoid initialization error
+  get movies() {
+    return this.movieService.movies();
+  }
+
+  selectMovie(movie: string) {
+    // this.selectedMovie.set(movie);
+    console.log(movie);
+    this.omdb.searchMovies_desc(movie).subscribe({
+     next:(res)=>this.selectedMovie.set(res),
+     error:(err)=>console.log("error")
+    })
   }
 
   clearSelection() {
