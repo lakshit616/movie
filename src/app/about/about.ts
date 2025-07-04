eb96c466cae8dfea03d916606245f17caf1970db
lakshit616 <lakshit1304khandelwal@gmail.com>
2025-07-04 08:41:15 +0530

    new updates

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Header } from '../header/header';
 @Component({
   selector: 'app-about',
-  imports: [Header],
+  standalone: true,
+  imports: [CommonModule,Header],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrl: './about.css',
 })
 export class About {
+mode = signal<'profile' | 'details'>('profile');
 
-}
+  current_mode(event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.mode.set(checked ? 'details' : 'profile');}}
